d51c5e2cf3973b7f9c8cb2debda9e4b60a17ee5f
masus <thomas.steinmann@protonmail.com>
2024-03-18 16:49:42 +0100

    Added EndOfRace dialog

--- lib/views/timer/post_start/button_end_race.dart
+++ lib/views/timer/post_start/button_end_race.dart
@@ -3,6 +3,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:regatta_timer/components/layouts/layout_buttons.dart';
 import 'package:regatta_timer/controllers/app_view_controller.dart';
 import 'package:regatta_timer/providers/settings_provider.dart';
+import 'package:regatta_timer/providers/timer_providers.dart';
+import 'package:regatta_timer/views/timer/post_start/end_of_race_dialog.dart';
 
 class EndRaceButton extends HookConsumerWidget {
   const EndRaceButton({super.key});
@@ -15,7 +17,15 @@ class EndRaceButton extends HookConsumerWidget {
       child: LayoutButton(
         text: "End Race",
         onPressed: () async {
-          Navigator.pop(context);
+          ref.read(timerController.notifier).stop();
+
+          await showDialog<void>(
+            context: context,
+            builder: (context) {
+              return const EndOfRaceDialog();
+            },
+          );
+
           await ref.read(appViewController.notifier).enterSetTimeState();
         },
         buttonColor: Theme.of(context).colorScheme.secondary,
